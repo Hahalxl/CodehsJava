a0d60efdf80912e357c90b01382fc22fb7cd13d2
Hahalxl <136863536+Hahalxl@users.noreply.github.com>
2025-03-01 16:59:55 -0500

    Create main.java

---
+++ Unit 1: Basic Java/1/6/main.java
@@ -0,0 +1,20 @@
+public class Art extends ConsoleProgram{
+
+    /*
+    
+             /)___
+          /     O|___
+         /           )
+        /       )___/
+
+    
+    */
+    public void run()
+    {
+        System.out.println("   /)___"); 
+        System.out.println("  /     O|___"); 
+        System.out.println(" /           )"); 
+        System.out.println("/       )___/"); 
+    }
+
+}
