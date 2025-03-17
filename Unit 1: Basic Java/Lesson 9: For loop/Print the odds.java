c0b0e85fd1c03ce27c03e32227307186c94a5e89
Hahalxl <136863536+Hahalxl@users.noreply.github.com>
2025-03-17 14:30:31 -0400

    Create Print the odds.java

---
+++ Unit 1: Basic Java/Lesson 9: For loop/Print the odds.java
@@ -0,0 +1,11 @@
+public class Odds extends ConsoleProgram
+{
+    public void run()
+    {
+        for(int i=0;i<100;i++){
+            if(i % 2 == 1){
+                System.out.println(i);
+            }
+        }
+    }
+}
