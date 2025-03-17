04d1e6b5e5c2f8f00757ee56f97ab10e84f853a9
Hahalxl <136863536+Hahalxl@users.noreply.github.com>
2025-03-17 14:25:36 -0400

    Create Ice cream.java

---
+++ Unit 1: Basic Java/Lesson 6: Booleans/Ice cream.java
@@ -0,0 +1,8 @@
+public class IceCream extends ConsoleProgram
+{
+    public void run()
+    {
+       boolean like = readBoolean("Do you like icecream? ");
+       System.out.println(like);
+    }
+}
