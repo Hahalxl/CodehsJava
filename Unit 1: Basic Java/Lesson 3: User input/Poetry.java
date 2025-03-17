86d10ef2063a2da36e198610a78580eb2a63e9cf
Hahalxl <136863536+Hahalxl@users.noreply.github.com>
2025-03-17 14:19:48 -0400

    Create Poetry.java

---
+++ Unit 1: Basic Java/Lesson 3: User input/Poetry.java
@@ -0,0 +1,9 @@
+public class Poetry extends ConsoleProgram
+{
+    public void run()
+    {
+    System.out.println("Money is earn through work\nSurvival require money\nWe can't live without");
+    System.out.println("Made for suffering\n A use where\n Tens and thousands\n Hours spent on this\n Suffer for me");
+    System.out.println(readLine("Give me your poem? "));
+    }
+}
