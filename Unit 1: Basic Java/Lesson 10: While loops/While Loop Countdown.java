60e448caa3198e87a9a49bc4ef827c8ba2f22ed9
Hahalxl <136863536+Hahalxl@users.noreply.github.com>
2025-03-18 19:33:45 -0400

    Create While Loop Countdown.java

---
+++ Unit 1: Basic Java/Lesson 10: While loops/While Loop Countdown.java
@@ -0,0 +1,13 @@
+public class WhileLoop extends ConsoleProgram
+{
+    public void run()
+    {
+        int i = 10;
+        
+        while(i >= 0)
+        {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
